3860de19b695c8203afabccbcbff2978b4f1b289
Les Aker <me@lesaker.org>
2014-03-26 21:15:04 -0400

    module boilerplate

---
+++ lib/octoauth.rb
@@ -0,0 +1,23 @@
+require 'octokit'
+
+##
+# Octokit wrapper for simplifying authentication
+module Octoauth
+  class << self
+    ##
+    # Insert a helper .new() method for creating a new Auth object
+    def new(*args)
+      self::Auth.new(*args)
+    end
+  end
+
+  ##
+  # Define some sane defaults
+  DEFAULTS = {
+  }
+
+  ##
+  # Authentication object
+  class Auth
+  end
+end
