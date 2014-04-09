848331cd20ca04bfc93102b78a921dce5c4062ee
Les Aker <me@lesaker.org>
2014-04-08 22:47:12 -0400

    this a loop

--- lib/octoauth/auth.rb
+++ lib/octoauth/auth.rb
@@ -2,5 +2,19 @@ module Octoauth
   ##
   # Authentication object
   class Auth
+    def initialize(params = {})
+      @config = Config.new params.subset(:file, :note)
+      @token = get_token
+    end
+
+    def get_token
+      return @config.token if @config.token && valid_scope?(@config.token)
+    end
+  end
+end
+
+class Hash
+  def subset(*keys)
+    select { |k| keys.include? k }
   end
 end
